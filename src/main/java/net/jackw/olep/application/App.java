9a430d971dab80abbae768838bd2f31defe0f414
Jack Wickham <jackwickham@live.co.uk>
2019-01-24 22:38:18 +0000

    Switch to CSV for metrics

--- src/main/java/net/jackw/olep/application/App.java
+++ src/main/java/net/jackw/olep/application/App.java
@@ -1,17 +1,32 @@
 package net.jackw.olep.application;
 
 import akka.actor.ActorSystem;
+import com.codahale.metrics.CsvReporter;
 import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.jmx.JmxReporter;
 import net.jackw.olep.common.KafkaConfig;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) {
         MetricRegistry registry = new MetricRegistry();
-        final JmxReporter reporter = JmxReporter.forRegistry(registry).build();
-        reporter.start();
+
+        CsvReporter reporter = CsvReporter.forRegistry(registry)
+            .formatFor(Locale.UK)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .build(new File("results/"));
+        // Wait a minute for the system to warm up, then collect metrics every 30s
+        reporter.start(60L, 30L, TimeUnit.SECONDS);
 
         ActorSystem system = ActorSystem.create("olep");
+
+        start(registry, system);
+    }
+
+    public static void start(MetricRegistry registry, ActorSystem system) {
         for (int i = 1; i < KafkaConfig.warehouseCount(); i += 10) {
             system.actorOf(TerminalGroup.props(i, 10, registry), "term-group-" + i);
         }
